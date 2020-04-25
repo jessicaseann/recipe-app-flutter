110f0c5b3abec41d5ee52acb4299c1551771abf8
josephgunawan97 <joseph.gunawan97@gmail.com>
2020-04-25 20:55:02 +0700

    Add Folder Structure

---
+++ lib/models/recipe.dart
@@ -0,0 +1,22 @@
+
+class Recipe {
+
+  final String id; 
+  final String image; 
+  final String title; 
+  final int readyInMinutes; 
+  final int servings; 
+
+  Recipe({this.title, this.id, this.image, this.servings, this.readyInMinutes});
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      id: json["id"],
+      title: json["title"], 
+      image: json["image"], 
+      readyInMinutes: json["readyInMinutes"], 
+      servings: json["servings"], 
+    );
+  }
+
+}
