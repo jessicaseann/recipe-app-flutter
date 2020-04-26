4136d070c7ad8f23001da87e9ca9f01724051e1f
Jessica Sean <jessica.sean@tokopedia.com>
2020-04-26 14:13:43 +0700

    revamp ui search page

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -1,72 +1,66 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:recipeappflutter/services/webservices.dart';
 import 'package:recipeappflutter/view_models/recipe_list_view_model.dart';
 import 'package:recipeappflutter/widgets/loading.dart';
 import 'package:recipeappflutter/widgets/recipe_list.dart';
 
 class SearchPage extends StatefulWidget {
-  @override 
-  _SearchPageState createState() => _SearchPageState(); 
+  @override
+  _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-
-  final TextEditingController _controller = TextEditingController(); 
+  final TextEditingController _controller = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     // you can uncomment this to get all batman movies when the page is loaded
-    Provider.of<RecipeListViewModel>(context, listen: false).fetchRandomRecipes();
+    Provider.of<RecipeListViewModel>(context, listen: false)
+        .fetchRandomRecipes();
   }
 
   @override
   Widget build(BuildContext context) {
-
     final data = Provider.of<RecipeListViewModel>(context);
     return Container(
-      padding: EdgeInsets.all(10),
-      child: Column(children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(top: 20),
-          padding: EdgeInsets.only(left: 10),
-          decoration: BoxDecoration(
-            color: Colors.grey, 
-            borderRadius: BorderRadius.circular(10)
+        padding: EdgeInsets.all(16),
+        child: Column(children: <Widget>[
+          Card(
+            margin: EdgeInsets.only(top: 36, bottom: 16),
+            child: Container(
+                padding: EdgeInsets.only(left: 16),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  controller: _controller,
+                  onSubmitted: (value) async {
+                    if (value.isNotEmpty) {
+                      await data.searchRecipe(value);
+                      setState(() {});
+                    }
+                  },
+                  style: TextStyle(color: Color.fromRGBO(250, 120, 130, 1)),
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    hintStyle:
+                        TextStyle(color: Color.fromRGBO(250, 120, 130, 0.5)),
+                    border: InputBorder.none
+                  ),
+                )),
           ),
-          child: TextField(
-            controller: _controller,
-            onSubmitted: (value) async {
-              if(value.isNotEmpty) {
-                await data.searchRecipe(value);
-                setState(() {
-                });
-              }
-            },
-            style: TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-              hintText: "Search", 
-              hintStyle: TextStyle(color: Colors.white),
-              border: InputBorder.none
-            ),
-          ),
-        ), 
-        Expanded(
-          child: _getData(data)
-        )
-      ])
-    );
+          Expanded(child: _getData(data))
+        ]));
   }
 
   Widget _getData(data) {
     print(data.loading);
-    if (!data.loading && data.recipes != null) 
+    if (!data.loading && data.recipes != null)
       return RecipeList(recipes: data.recipes);
-    else if (data.loading)  
+    else if (data.loading)
       return Loading();
-    else 
-      return Text('Something Wrong');      
+    else
+      return Text('Something Wrong');
   }
 }
--- lib/widgets/recipe_list.dart
+++ lib/widgets/recipe_list.dart
@@ -2,33 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:recipeappflutter/view_models/recipe_view_model.dart';
 
 class RecipeList extends StatelessWidget {
-
-  final List<RecipeViewModel> recipes; 
+  final List<RecipeViewModel> recipes;
 
   RecipeList({this.recipes});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      padding: EdgeInsets.symmetric(vertical: 8),
       itemCount: this.recipes.length,
       itemBuilder: (context, index) {
-        
         final recipe = this.recipes[index];
 
-        return ListTile(
-          contentPadding: EdgeInsets.all(10),
-          leading: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: recipe.image != null ? NetworkImage('https://spoonacular.com/recipeImages/${recipe.id}-90x90.jpg') : NetworkImage('https://i.picsum.photos/id/102/250/250.jpg')
-              ),
-              borderRadius: BorderRadius.circular(6)
-            ),
-            width: 50, 
-            height: 100,
-            ),
-          title: Text(recipe.title),
+        return Container(
+          height: 100,
+          child: Card(
+              clipBehavior: Clip.antiAlias,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Image(
+                        fit: BoxFit.cover,
+                        image: recipe.image != null
+                            ? NetworkImage(
+                                'https://spoonacular.com/recipeImages/${recipe.id}-90x90.jpg')
+                            : NetworkImage(
+                                'https://i.picsum.photos/id/102/250/250.jpg')),
+                    width: 80,
+                    height: 100,
+                  ),
+                  Flexible(
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            recipe.title,
+                            style: TextStyle(fontSize: 18, letterSpacing: 1.2),
+                            maxLines: 1,
+                          ),
+                          Text("Ready in ${recipe.readyInMinutes} min(s)")
+                        ],
+                      ),
+                    )
+                  )
+                ],
+              )),
         );
       },
     );
